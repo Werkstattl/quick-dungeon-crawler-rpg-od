14adc6d4d173edc6b4cfcf181c227047aadfe7d0
7underlines <7underlines@gmail.com>
2025-07-26 19:09:08 +0200

    Add dodge stat to forge equipment stats display

--- assets/js/forge.js
+++ assets/js/forge.js
@@ -130,7 +130,7 @@ const loadForgeEquipment = () => {
         const statsHtml = equip.stats.map(stat => {
             const statName = Object.keys(stat)[0];
             const statValue = stat[statName];
-            if (["critRate", "critDmg", "atkSpd", "vamp"].includes(statName)) {
+            if (["critRate", "critDmg", "atkSpd", "vamp", "dodge"].includes(statName)) {
                 return `<li>${statName.replace(/([A-Z])/g, ".$1").replace(/crit/g, "c").toUpperCase()}+${statValue.toFixed(2).replace(rx, "$1")}%</li>`;
             } else {
                 return `<li>${statName.replace(/([A-Z])/g, ".$1").replace(/crit/g, "c").toUpperCase()}+${statValue}</li>`;
