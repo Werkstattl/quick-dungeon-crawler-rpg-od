61b69f85dec3f1bb88b06babe16060d31f2a01c4
7underlines <thomas.peissl@logmedia.at>
2025-07-31 10:59:32 +0200

    increase inventory limit 99

--- assets/js/player.js
+++ assets/js/player.js
@@ -19,7 +19,7 @@ if (player) {
 let inventoryOpen = false;
 let leveled = false;
 
-const MAX_INVENTORY_ITEMS = 40;
+const MAX_INVENTORY_ITEMS = 99;
 
 const inventoryItemCount = () => {
     const consumables = player.inventory && player.inventory.consumables ? player.inventory.consumables.length : 0;
