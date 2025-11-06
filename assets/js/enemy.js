ebfbd6b4e381f7d866dbf4a7ada07e79d259f97f
7underlines <thomas.peissl@logmedia.at>
2025-11-06 21:40:39 +0100

    firt enemy change

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -143,10 +143,6 @@ const generateRandomEnemy = (condition) => {
     } else {
         enemy.lvl = randomizeNum(minLvl, maxLvl);
     }
-    if (player && player.lvl === 1 && enemy.lvl > 3) {
-        enemy.lvl = 3;
-    }
-
     let pool = enemyPools[enemy.type].base;
     if (condition === 'guardian') {
         pool = enemyPools[enemy.type].guardian;
