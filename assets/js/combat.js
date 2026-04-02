6578a21336396c59832ddd3921a39e68ae3d2ab0
7underlines <7underlines@gmail.com>
2026-04-02 20:42:22 +0000

    victory

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -1057,6 +1057,7 @@ const bindRunSummaryButton = () => {
 const handleSpecialBossVictory = () => {
     pendingRunSummary = createRunSummary('victory');
     runSummaryReadyInCombat = true;
+    addCombatLog("<span class='Legendary'>🏆 Victory! The Dungeon Monarch has been defeated.</span>");
     dungeon.status.exploring = false;
     dungeon.status.paused = true;
     dungeon.status.event = false;
@@ -1071,8 +1072,7 @@ const handleSpecialBossVictory = () => {
         clearInterval(playTimer);
         playTimer = null;
     }
-
-    updateCombatLog();
+	updateCombatLog();
 };
 
 const hasSellableCombatLoot = () => {
